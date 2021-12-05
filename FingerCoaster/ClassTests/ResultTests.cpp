b781410c4ebd5e0b5cb739fd3a098f1db2e5ce61
Pavle Vlajkovic <mi17298@matf.bg.ac.rs>
2021-12-05 11:35:47 +0100

    Klasa Result dodata
    Result zavrsen, testovi odradjeni.

---
+++ FingerCoaster/ClassTests/ResultTests.cpp
@@ -0,0 +1,45 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "../Result.hpp"
+
+#include <ctime>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+TEST_CASE("Result class tests"){
+
+    SECTION("Constructor - Wpm out of bounds throws exception"){
+
+            time_t rawTime;
+            struct tm* date;
+            unsigned wpm = 500;
+            int correct = 50;
+            int failed = 30;
+            std::string username = "username";
+
+            rawTime = time(0);
+            date = localtime(&rawTime);
+            
+            REQUIRE_THROWS(Result(*date,wpm,correct,failed,username));
+    }
+
+    SECTION("Format - No empty string"){
+        time_t rawTime;
+            struct tm* date;
+            unsigned wpm = 100;
+            int correct = 53;
+            int failed = 31;
+            std::string username = "username";
+
+            rawTime = time(0);
+            date = localtime(&rawTime);
+            Result result(*date,wpm,correct,failed,username);
+            //std::string formatedOutput = result.formatResult();
+            
+            std::cout << formatedOutput << std::endl;
+            CHECK(formatedOutput.size() > 0);
+    }
+
+}
--- FingerCoaster/Result.hpp
+++ FingerCoaster/Result.hpp
@@ -1,39 +1,69 @@
 #ifndef STUDENT_HPP
 #define STUDENT_HPP
 
-#include<ctime>
-#include<string>
+#include <ctime>
+#include <string>
+#include <stdexcept>
+#include <iomanip>
 
 class Result{
 
 public:
-    Result(std::tm& date,unsigned wpm,float accuracy,std::string username){
+    Result(std::tm& date,unsigned wpm,int correct,int failed,std::string username){
 
+        if(wpm > 300){ 
+            throw std::invalid_argument("Wpm out of range");
+        }
         
-        
-
+        _date = date;
+        _wpm = wpm;
+        _correct = correct;
+        _failed = failed;
+        _username = username;
+    }
+    
+    unsigned giveWpm() const{
+        return _wpm;
     }
 
+    std::string formatResult() {
+
+        std::stringstream formatedResult;
+
+        formatedResult << std::fixed << std::setprecision(2)
+                       << _date.tm_mday
+                       << ":"
+                       <<  _date.tm_mon + 1
+                       << ":"
+                       << _date.tm_year + 1900
+                       << " - "
+                       << "Wpm:" << _wpm << " - "
+                       << "Accuracy:" << calcAccuracy() << "% - "
+                       << "Correct:" << _correct << " - "
+                       << "Failed:" << _failed << " - "
+                       << "Username:" << _username << std::endl;
+                                     
+       return formatedResult.str();
+    }
+
+private:
+    float calcAccuracy(){
+        
+        int totalWords = _correct + _failed;
+
+        if(totalWords == 0)
+            return 0.0;
+
+        return (_correct * 100) / float(totalWords); 
+    }
 
 private:
     std::tm _date;
     unsigned _wpm;
-    float  _accuracy;
+    int _correct;
+    int _failed;
     std::string _username;
-    
+
 };
 
 #endif
-
-
-//Atributi
-//date - date
-//wpm - short
-//percentage - float
-//correct - short 
-//failed - short 
-//user - User
-//--------
-//getwpm - Short 
-//getruslt - String
-
