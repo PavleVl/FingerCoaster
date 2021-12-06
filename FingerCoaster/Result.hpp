4a6ca523284ec6c5a40ca2ff653ee26651ebf5cc
Pavle Vlajkovic <mi17298@matf.bg.ac.rs>
2021-12-06 14:59:49 +0100

    Result klasa promene
    Dodat je jos jedan konstruktor i promenjen je nacin na koji
    ce se cuvati datum, tj bice u formatu d:m:godina i to u
    tipu string

--- FingerCoaster/Result.hpp
+++ FingerCoaster/Result.hpp
@@ -1,18 +1,18 @@
 #ifndef STUDENT_HPP
 #define STUDENT_HPP
 
-#include <ctime>
 #include <string>
 #include <stdexcept>
 #include <iomanip>
 
+
 class Result{
 
 public:
-    Result(std::tm& date,unsigned wpm,int correct,int failed,std::string username){
+    Result(std::string date,unsigned wpm,int correct,int failed,std::string username){
 
         if(wpm > 300){ 
-            throw std::invalid_argument("Wpm out of range");
+            throw std::invalid_argument("Wpm out of range!");
         }
         
         _date = date;
@@ -22,21 +22,30 @@ public:
         _username = username;
     }
     
+    Result(std::vector<std::string>& formatedResult){
+
+        if(formatedResult.size() == 0){
+            throw std::invalid_argument("Result class arguments missing!");
+        }
+
+
+        _date = formatedResult[0];
+        _wpm = std::stoul(formatedResult[1]);
+        _correct = std::stoi(formatedResult[3]);
+        _failed = std::stoi(formatedResult[4]);
+        _username = formatedResult[5];
+    }
+
     unsigned giveWpm() const{
         return _wpm;
     }
 
-    std::string formatResult() {
+    std::string formatResultDisplay() {
 
         std::stringstream formatedResult;
 
         formatedResult << std::fixed << std::setprecision(2)
-                       << _date.tm_mday
-                       << ":"
-                       <<  _date.tm_mon + 1
-                       << ":"
-                       << _date.tm_year + 1900
-                       << " - "
+                       << _date << " - "
                        << "Wpm:" << _wpm << " - "
                        << "Accuracy:" << calcAccuracy() << "% - "
                        << "Correct:" << _correct << " - "
@@ -46,7 +55,22 @@ public:
        return formatedResult.str();
     }
 
-private:
+    std::string formatResultSave(){
+
+        std::stringstream formatedResult;
+
+        formatedResult << std::fixed << std::setprecision(2)
+                       << _date
+                       << "-"
+                       << _wpm << "-"
+                       << calcAccuracy() << "-"
+                       << _correct << "-"
+                       << _failed << "-"
+                       << _username << std::endl;
+
+        return formatedResult.str();
+    }
+
     float calcAccuracy(){
         
         int totalWords = _correct + _failed;
@@ -58,7 +82,7 @@ private:
     }
 
 private:
-    std::tm _date;
+    std::string _date;
     unsigned _wpm;
     int _correct;
     int _failed;
