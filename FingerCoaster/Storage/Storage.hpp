f47059e3642e51a55495492cd8100d4218bcc934
Pavle Vlajkovic <mi17298@matf.bg.ac.rs>
2021-12-17 08:21:14 +0100

    Skelet klase Storage napravljen

---
+++ FingerCoaster/Storage/Storage.hpp
@@ -0,0 +1,24 @@
+#ifndef STORAGE_HPP
+#define STORAGE_HPP
+
+
+
+
+class Storage{
+
+
+
+
+};
+
+#endif
+
+// Treba nam:
+// Seter/Geter za tezinu
+// Seter/Geter za broj igraca
+// Seter/Geter za adresu servera
+
+// Putanje do tekstova
+// Ucitavanje tekstova
+// Obrada tekstova u vektor
+// Odabir reci za igru
---
+++ FingerCoaster/Storage/StorageTests.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "../SavedFiles/catch.hpp"
+#include "Storage.hpp"
+
+TEST_CASE("Storage class tests"){
+
+    Section("Something"){
+
+
+    }
+}
