9056416d7e9cab7a65270bbca9a0bec00718a6db
VladimirMandic10 <vladimir.mandaleone10@gmail.com>
2021-12-19 19:05:40 +0100

    kostur klijenta

--- FingerCoaster/Client/Client.cpp
+++ FingerCoaster/Client/Client.cpp
@@ -1,17 +1,29 @@
+#include "Client.h"
 
-#include "Client.hpp"
-
-#define DEFAULT_PORT = 1234
-
-void Client::connectToServer(){
-
-    socketFd = socket(AF_INET6,SOCK_DGRAM,0);
-
-    if(socketFd == -1){
-        std::strer
-    }
-
-    
-    
-
+Client::Client(QString name,QObject *parent)
+    : QObject{parent},
+      name(name)
+{
+    connect(socket,SIGNAL(socket->connected()),this,SLOT(connected()));
+    connect(socket,SIGNAL(socket->disconnected()),this,SLOT(disconnected()));
 }
+
+void Client::connectToHost(const QHostAddress &addr, quint16 port){
+    socket->connectToHost(addr,port);
+    address = addr;
+    this->port = port;
+}
+
+void Client::disconnectFromHost(){
+    socket->disconnectFromHost();
+    qDebug()<<"Disconnected";
+}
+
+void Client::connected(){
+    qDebug()<<"Connected to host";
+}
+
+
+
+
+
