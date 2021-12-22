3ee84fa998996fe820b0f798aa9343ccb967ca58
VladimirMandic10 <vladimir.mandaleone10@gmail.com>
2021-12-22 15:22:50 +0100

    Konekcija klijenta sa localhostom

--- FingerCoaster/Client/Client.cpp
+++ FingerCoaster/Client/Client.cpp
@@ -1,29 +1,38 @@
-#include "Client.h"
+#include "Client.hpp"
 
 Client::Client(QString name,QObject *parent)
     : QObject{parent},
-      name(name)
+      name(name),
+      tcpSocket(new QTcpSocket(this))
 {
-    connect(socket,SIGNAL(socket->connected()),this,SLOT(connected()));
-    connect(socket,SIGNAL(socket->disconnected()),this,SLOT(disconnected()));
-}
+    connect(tcpSocket,SIGNAL(connected()),this,SLOT(connectedCl()));
+    connect(tcpSocket,SIGNAL(disconnected()),this,SLOT(disconnectedCl()));
+    connect(tcpSocket,&QAbstractSocket::errorOccurred,this,&Client::printError);
+    qDebug()<<" Connecting...";
+    tcpSocket->connectToHost(QHostAddress::LocalHost,12345);
 
-void Client::connectToHost(const QHostAddress &addr, quint16 port){
-    socket->connectToHost(addr,port);
-    address = addr;
-    this->port = port;
-}
-
-void Client::disconnectFromHost(){
-    socket->disconnectFromHost();
-    qDebug()<<"Disconnected";
-}
-
-void Client::connected(){
-    qDebug()<<"Connected to host";
 }
 
 
+void Client::connectedCl(){
+    qDebug()<<"Connected!";
+}
+
+void Client::disconnectedCl(){
+    tcpSocket->close();
+    qDebug()<<"Disconnected from host";
+}
 
 
-
+void Client::printError(QAbstractSocket::SocketError socketError){
+    switch(socketError){
+        case QAbstractSocket::RemoteHostClosedError:break;
+        case QAbstractSocket::HostNotFoundError:
+              qDebug()<<"The host was not found,please check the hostname and port";break;
+        case QAbstractSocket::ConnectionRefusedError:
+            qDebug()<<"The connection was refused. "
+                      "Make sure the server is running "
+                      "and check that the hostname and port settings are correct";break;
+        default:qDebug()<<"The following error occured:%1."<<tcpSocket->errorString();
+    }
+}
